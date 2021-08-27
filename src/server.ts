2ab8a8229c5094646e2324f8a6f0a787bbe4fdd9
Luiz Arthur <luizarthurvm@gmail.com>
2021-08-26 21:49:03 -0300

    Padrões fixed

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'hello Gostack' }));
+app.get('/', (request, response) =>
+    response.json({ message: 'hello Gostack' }),
+);
 
 app.listen(3333, () => {
     console.log('Server started');
