4328a3a4b96355c23696124979ba446b968dc54f
Luiz Arthur <luizarthurvm@gmail.com>
2021-08-26 21:37:50 -0300

    Padrões

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: "hello Gostack" });
-});
+app.get('/', (request, response) => response.json({ message: 'hello Gostack' }));
 
 app.listen(3333, () => {
-    console.log("Server started")
+    console.log('Server started');
 });
