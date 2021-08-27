1412a4003ecfa4c39dbe634737f1097e4918e29d
Luiz Arthur <luizarthurvm@gmail.com>
2021-08-27 14:47:54 -0300

    Model e Data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) =>
-    response.json({ message: 'hello Gostack' }),
-);
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server started');
+    console.log('🚀 Server started on port 3333');
 });
