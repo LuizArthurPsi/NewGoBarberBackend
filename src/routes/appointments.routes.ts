c9190ebbb0c144738fc9ea51044c12d02ac60427
Luiz Arthur <luizarthurvm@gmail.com>
2021-08-30 17:54:11 -0300

    Listando agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,11 +1,17 @@
 // Criar, deletar, modificar infos (CRUD) fica no repositórios, realiza as operações em cima dos dados armazenados
 
-import { Router } from 'express';
+import { request, Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+
+    return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
